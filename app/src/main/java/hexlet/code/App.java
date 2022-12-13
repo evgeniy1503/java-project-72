11480f5187e3fbf53f2f586c7d2ae76d2bb5fe31
evgeniy1503 <evgeniy15031991@mail.ru>
2022-12-13 21:39:41 +0300

    fixed deploy

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,7 +6,10 @@ import io.javalin.Javalin;
 
 public  class App {
 
-
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "5000");
+        return Integer.valueOf(port);
+    }
 
     public static Javalin getApp() {
         Javalin app = Javalin.create(config -> {
@@ -19,6 +22,6 @@ public  class App {
 
     public static void main(String[] args) {
         Javalin app = getApp();
-        app.start(5000);
+        app.start(getPort());
     }
 }
